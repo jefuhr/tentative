f57db393b567239a8e735ee3f77069d00e110b21
moppr <mantony42@gmail.com>
2021-03-04 22:34:46 -0500

    add broker

---
+++ broker/brokerRabbitMQServer.php
@@ -0,0 +1,49 @@
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+function doLogin($username,$password)
+{
+    // lookup username in databas
+    // check password
+    return true;
+    //return false if not valid
+}
+
+function doDBRequest($request)
+{
+  $client = new rabbitMQClient("dbRabbitMQ.ini","testServer");
+  $response = $client->send_request($request);
+  return $response;
+}
+
+function requestProcessor($request)
+{
+  echo "received request".PHP_EOL;
+  var_dump($request);
+  if(!isset($request['type']))
+  {
+    return "ERROR: unsupported message type";
+  }
+  switch ($request['type'])
+  {
+    case "login":
+      return doLogin($request['username'],$request['password']);
+    case "database_request":
+      return doDBRequest($request);
+    case "validate_session":
+      return doValidate($request['sessionId']);
+  }
+  return array("returnCode" => '0', 'message'=>"Server received request and processed");
+}
+
+$server = new rabbitMQServer("brokerRabbitMQ.ini","testServer");
+
+echo "testRabbitMQServer BEGIN".PHP_EOL;
+$server->process_requests('requestProcessor');
+echo "testRabbitMQServer END".PHP_EOL;
+exit();
+?>
+
