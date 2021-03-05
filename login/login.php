08d1f421fa108aca1d95fc54c449ed43f2cfd051
Jacob Fuhr <jefuhr98@gmail.com>
2021-03-04 20:04:10 -0500

    .

--- login/login.php
+++ login/login.php
@@ -3,15 +3,24 @@
 
 session_start();
 include ("config.php");
+$json = file_get_contents("login_user.json");
+$json_data = json_decode($json, true);
+$db= //insert database
+$username= $json_data["username"];
+$password= $json_data["password"];
 
-$db= ; //insert database
-$username=$_GET["username"];
-$password=$_GET["password"];
-
-function auth ($username, $password, $db)
+function auth ($username, $password, $db) {
 	global $query;
 	$temp = "select * from playerData where username = '$username' ";
 
-
-
+	($query = mysqli_query ($db, $temp)) or die (mysqli_error ($db));
+	$hash = $r['pass'];
+	if (password_verify($password, $hash)){
+		echo "Good Password";
+		return true;
+	} else { 
+		echo "Bad Password";
+		return false;
+	}
+}	
 ?> 
