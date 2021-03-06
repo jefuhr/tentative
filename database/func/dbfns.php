a3604c1335af0895fe6257d9aaccc853ae759146
Cam Ortillo <camortillo@gmail.com>
2021-03-05 21:17:34 -0500

    adjusted error handling

--- database/func/dbfns.php
+++ database/func/dbfns.php
@@ -1,11 +1,16 @@
 <?php
+  mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
   function login( $username, $password ) { // Returns a JSON object based on if user info is valid
     global $db;
     global $userData;
 
     $s = "SELECT * FROM `$userData` WHERE username='$username' AND password='$password'"; 
-    ( $t = mysqli_query($db, $s) ) or die ( return_json( "503", "Database error occured while authenticating login." ) );
-    $num = mysqli_num_rows ( $t );
+    try {
+      ( $t = mysqli_query($db, $s) ); // or die ( return_json( "503", "Database error occured while authenticating login." ) );
+    } catch (Exception $e) {
+      return_json( "503", "Database error occured while authenticating login." );
+    }
+      $num = mysqli_num_rows ( $t );
 
     if( $num >= 1 ) {
       while ( $r = mysqli_fetch_array ( $t, MYSQLI_ASSOC) ) {
@@ -21,8 +26,12 @@
     global $userData;
 
     $s = "INSERT INTO `$userData` (`username`, `password`) VALUES ('$username', '$password')"; 
-    ( $t = mysqli_query($db, $s) ) or die ( return_json( "503", "Database error occured while registering new user." ) );
-    $num = mysqli_num_rows ( $t );
+    try {
+      ( $t = mysqli_query($db, $s) ); // or die ( return_json( "503", "Database error occured while registering new user." ) );
+    } catch (Exception $e)  {
+      return_json( "503", "Database error occured while registering new user." );
+    }
+      $num = mysqli_num_rows ( $t );
     return return_json( "200", "Sucessfully registered new user.");
   }
 
@@ -31,8 +40,12 @@
     global $userData;
 
     $s = "SELECT * FROM `$userData` WHERE uuid='$uuid'"; 
-    ( $t = mysqli_query($db, $s) ) or die ( return_json( "503", "Database error occured while fetching username." ) );
-    $num = mysqli_num_rows ( $t );
+    try {
+      ( $t = mysqli_query($db, $s) ); // or die ( return_json( "503", "Database error occured while fetching username." ) );
+    } catch (Exception $e)  {
+      return_json( "503", "Database error occured while fetching username." );
+    }
+      $num = mysqli_num_rows ( $t );
 
     if( $num >= 1 ) {
       while ( $r = mysqli_fetch_array ( $t, MYSQLI_ASSOC) ) {
@@ -48,8 +61,12 @@
     global $currencyData;
 
     $s = "SELECT * FROM `$currencyData` WHERE currencyType='$currencyType'"; 
-    ( $t = mysqli_query($db, $s) ) or die ( return_json( "503", "Database error occured while fetching currency data." ) );
-    $num = mysqli_num_rows ( $t );
+    try {
+      ( $t = mysqli_query($db, $s) ); // or die ( return_json( "503", "Database error occured while fetching currency data." ) );
+    } catch (Exception $e)  {
+      return_json( "503", "Database error occured while fetching currency data." );
+    }
+      $num = mysqli_num_rows ( $t );
 
     if( $num >= 1 ) {
       while ( $r = mysqli_fetch_array ( $t, MYSQLI_ASSOC) ) {
@@ -64,8 +81,12 @@
     global $db;
     global $currencyData;
     $s = "UPDATE `$currencyData` SET `currentValue`='$currentValue' WHERE `currencyType`='$currencyType'";
-    ( $t = mysqli_query($db, $s) ) or die ( return_json( "503", "Database error occured while updating resources for Player: $uuid." ) );
-    return return_json("200", "Successfully updated currentValue for currencyType: $currencyType.");
+    try {
+      ( $t = mysqli_query($db, $s) ); // or die ( return_json( "503", "Database error occured while updating resources for Player: $uuid." ) );
+    } catch (Exception $e)  {
+      return_json( "503", "Database error occured while updating resources for Player: $uuid." );
+    }
+      return return_json("200", "Successfully updated currentValue for currencyType: $currencyType.");
 
   }
 
@@ -74,8 +95,12 @@
     global $currencyData;
 
     $s = "SELECT * FROM `$currencyData`"; 
-    ( $t = mysqli_query($db, $s) ) or die ( return_json( "503", "Database error occured while fetching all currency data." ) );
-    $num = mysqli_num_rows ( $t );
+    try {
+      ( $t = mysqli_query($db, $s) ); // or die ( return_json( "503", "Database error occured while fetching all currency data." ) );
+    } catch (Exception $e)  {
+      return_json( "503", "Database error occured while fetching all currency data." );
+    }
+      $num = mysqli_num_rows ( $t );
 
     $all = [];
     $id = 0;
@@ -92,8 +117,12 @@
     global $tradingData;
 
     $s = "SELECT * FROM `$currencyData`"; 
-    ( $t = mysqli_query($db, $s) ) or die ( return_json( "503", "Database error occured while fetching all trade data." ) );
-    $num = mysqli_num_rows ( $t );
+    try {
+      ( $t = mysqli_query($db, $s) ); // or die ( return_json( "503", "Database error occured while fetching all trade data." ) );
+    } catch (Exception $e)  {
+      return_json( "503", "Database error occured while fetching all trade data." );
+    }
+      $num = mysqli_num_rows ( $t );
 
     $all = [];
     $id = 0;
@@ -113,8 +142,12 @@
     global $tradingData;
 
     $s = "INSERT INTO `$tradingData`(`uuid`, `itemType`, `itemQuant`, `requestType`, `requestQuant`) VALUES ('$uuid', '$itemType', '$itemQuant', '$requestType', '$requestQuant')";
-    ( $t = mysqli_query($db, $s) ) or die ( return_json( "503", "Database error occured while submitting trade for Player: $uuid." ) );
-    return return_json( "200", "Successfully submitted trade for Player: $uuid to marketplace." );
+    try {
+      ( $t = mysqli_query($db, $s) ); // or die ( return_json( "503", "Database error occured while submitting trade for Player: $uuid." ) );
+    } catch (Exception $e)  {
+      return_json( "503", "Database error occured while submitting trade for Player: $uuid." );
+    }
+      return return_json( "200", "Successfully submitted trade for Player: $uuid to marketplace." );
   }
 
   function delete_trade( $tradeID ) { // delete trade from marketplace
@@ -122,8 +155,12 @@
     global $tradingData;
 
     $s = "DELETE FROM `$tradingData` WHERE tradeID='$tradeID'";
-    ( $t = mysqli_query($db, $s) ) or die ( return_json( "503", "Database error occured while deleting Trade: $tradeID." ) );
-    return return_json( "200", "Successfully deleted Trade: $tradeID from marketplace." ); 
+    try {
+      ( $t = mysqli_query($db, $s) ); // or die ( return_json( "503", "Database error occured while deleting Trade: $tradeID." ) );
+    } catch (Exception $e)  {
+      return_json( "503", "Database error occured while deleting Trade: $tradeID." );
+    }
+      return return_json( "200", "Successfully deleted Trade: $tradeID from marketplace." ); 
   }
 
   function update_player_resources( $uuid, $food, $wood, $stone, $leather, $iron, $gold, $currency0, $currency1, $currency2 ) { // Update player resource values
@@ -131,8 +168,12 @@
     global $userData;
 
     $s = "UPDATE `$userData` SET `food`='$food', `wood`='$wood', `stone`='$stone', `leather`='$leather', `iron`='$iron', `gold`='$gold', `currency0`='$currency0', `currency1`='$currency1', `currency2`='$currency2' WHERE `uuid`='$uuid'";
-    ( $t = mysqli_query($db, $s) ) or die ( return_json( "503", "Database error occured while updating resources for Player: $uuid." ) );
-    return return_json("200", "Successfully updated resources for Player: $uuid.");
+    try {
+      ( $t = mysqli_query($db, $s) ); // or die ( return_json( "503", "Database error occured while updating resources for Player: $uuid." ) );
+    } catch (Exception $e)  {
+      return return_json( "503", "Database error occured while updating resources for Player: $uuid." );
+    }
+      return return_json("200", "Successfully updated resources for Player: $uuid.");
   }
 
   function add_forum_post( $uuid, $topic, $message ){
@@ -140,12 +181,20 @@
     global $forumData;
 
     $s = "SELECT * FROM `$forumData` WHERE topic IS NOT NULL"; 
-    ( $t = mysqli_query($db, $s) ) or die ( return_json( "503", "Database error occured while submitting forum post for Player: $uuid." ) );
-    $num = mysqli_num_rows ( $t );
+    try {
+      ( $t = mysqli_query($db, $s) ); // or die ( return_json( "503", "Database error occured while submitting forum post for Player: $uuid." ) );
+    } catch (Exception $e)  {
+      return_json( "503", "Database error occured while submitting forum post for Player: $uuid." );
+    }
+      $num = mysqli_num_rows ( $t );
 
     $s = "INSERT INTO `$forumData` (`uuid`, `topicID`, `topic`, `message`) VALUES ('$uuid', '$num', '$topic', '$message')";
-    ( $t = mysqli_query($db, $s) ) or die ( return_json( "503", "Database error occured while submitting forum post for Player: $uuid." ) );
-    return return_json( "200", "Successfully submitted forum post for Player: $uuid to forum." );
+    try {
+      ( $t = mysqli_query($db, $s) ); // or die ( return_json( "503", "Database error occured while submitting forum post for Player: $uuid." ) );
+    } catch (Exception $e)  {
+      return_json( "503", "Database error occured while submitting forum post for Player: $uuid." );
+    }
+      return return_json( "200", "Successfully submitted forum post for Player: $uuid to forum." );
   }
 
   function add_forum_reply( $uuid, $topicID, $message ){
@@ -153,8 +202,12 @@
     global $forumData;
     
     $s = "INSERT INTO `$forumData` (`uuid`, `topicID`, `topic`, `message`) VALUES ('$uuid', '$topicID', NULL, '$message')";
-    ( $t = mysqli_query($db, $s) ) or die ( return_json( "503", "Database error occured while submitting forum reply for Player: $uuid." ) );
-    return return_json( "200", "Successfully submitted forum reply for Player: $uuid to forum." );
+    try {
+      ( $t = mysqli_query($db, $s) ); // or die ( return_json( "503", "Database error occured while submitting forum reply for Player: $uuid." ) );
+    } catch (Exception $e)  {
+      return_json( "503", "Database error occured while submitting forum reply for Player: $uuid." );
+    }
+      return return_json( "200", "Successfully submitted forum reply for Player: $uuid to forum." );
   }
 
   function get_all_forum_topics() {
@@ -162,10 +215,14 @@
     global $forumData;
 
     $s = "SELECT * FROM `$forumData` WHERE topic IS NOT NULL"; 
-    ( $t = mysqli_query($db, $s) ) or die ( return_json( "503", "Database error occured while fetching all forum topics." ) );
-    $num = mysqli_num_rows ( $t );
+    try {
+      ( $t = mysqli_query($db, $s) ); // or die ( return_json( "503", "Database error occured while fetching all forum topics." ) );
+    } catch (Exception $e)  {
+      return_json( "503", "Database error occured while fetching all forum topics." );
+    }
+      $num = mysqli_num_rows ( $t );
 
-    $all = [];
+    $all = []; 
     $id = 0;
     while ( $r = mysqli_fetch_array ( $t, MYSQLI_ASSOC) ) {
       $forumData = convert_forum_to_json( $r );
@@ -183,8 +240,12 @@
     global $forumData;
 
     $s = "SELECT * FROM `$forumData` WHERE topicID='$topicID'"; 
-    ( $t = mysqli_query($db, $s) ) or die ( return_json( "503", "Database error occured while fetching all forum topics." ) );
-    $num = mysqli_num_rows ( $t );
+    try {
+      ( $t = mysqli_query($db, $s) ); // or die ( return_json( "503", "Database error occured while fetching all forum topics." ) );
+    } catch (Exception $e)  {
+      return_json( "503", "Database error occured while fetching all forum topics." );
+    }
+      $num = mysqli_num_rows ( $t );
 
     $all = [];
     $id = 0;
