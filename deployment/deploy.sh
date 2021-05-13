b35a6a8276feedf5c143b20f07b5223b3c9e90fd
Jacob Fuhr <jefuhr98@gmail.com>
2021-05-13 09:54:16 -0400

    updates

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,48 +1,72 @@
 #! /bin/bash
 
 #installs dependences for tentive 
-SERVER="10.192.242.189"
+SERVER="10.192.243.172"
 USERNAME="jacob"
 PASSWORD="password" 
 TEST="Completed"
-
+mkdir -v -p $PWD/deploy
 echo 1 for Development, 2 for QA, 3 for Production
 read VERISON
 echo 1 for Client, 2 for Backend
+read WHAT
 
-mkdir -v -p $PWD/deploy
 
 if [[ $VERISON -eq 1 ]] 
 then
+	if [[ $WHAT -eq 2 ]]
+	then
+		scp -r $USERNAME@$SERVER:tentative/deployment/dependences $PWD/deploy
 
-	scp -r $USERNAME@$SERVER:tentative/deployment/dependences $PWD/deploy
+		#apt update 
+		dpkg -i $PWD/deploy/dependences/apacheds-2.0.0.AM26-amd64.deb
+		dpkg -i $PWD/deploy/dependences/php_7.2+60ubuntu1_all.deb
+		dpkg -i $PWD/deploy/dependences/mysql-apt-config_0.8.17-1_all.deb
+		dpkg -i $PWD/deploy/dependences/rabbitmq-server_3.8.14-1_all.deb
+		
+	elif [[ $WHAT -eq 1 ]]
+	then 
+		git clone https://github.com/jefuhr/tentative.git ./$PWD/deploy
+		
 
-	#apt update 
-	dpkg -i $PWD/deploy/dependences/apacheds-2.0.0.AM26-amd64.deb
-	dpkg -i $PWD/deploy/dependences/php_7.2+60ubuntu1_all.deb
-	dpkg -i $PWD/deploy/dependences/mysql-apt-config_0.8.17-1_all.deb
-	dpkg -i $PWD/deploy/dependences/rabbitmq-server_3.8.14-1_all.deb
+	fi
 fi
 if [[ $VERISON -eq 2 ]] 
 then
+	if [[ $WHAT -eq 2 ]]
+	then
+		scp -r $USERNAME@$SERVER:tentative/deployment/dependences $PWD/deploy
 
-	scp -r $USERNAME@$SERVER:tentative/deployment/dependences $PWD/deploy
+		#apt update 
+		dpkg -i $PWD/deploy/dependences/apacheds-2.0.0.AM26-amd64.deb
+		dpkg -i $PWD/deploy/dependences/php_7.2+60ubuntu1_all.deb
+		dpkg -i $PWD/deploy/dependences/mysql-apt-config_0.8.17-1_all.deb
+		dpkg -i $PWD/deploy/dependences/rabbitmq-server_3.8.14-1_all.deb
+		
+	elif [[ $WHAT -eq 1 ]]
+	then 
+		git clone https://github.com/jefuhr/tentative.git ./$PWD/deploy
+		
 
-	#apt update 
-	dpkg -i $PWD/deploy/dependences/apacheds-2.0.0.AM26-amd64.deb
-	dpkg -i $PWD/deploy/dependences/php_7.2+60ubuntu1_all.deb
-	dpkg -i $PWD/deploy/dependences/mysql-apt-config_0.8.17-1_all.deb
-	dpkg -i $PWD/deploy/dependences/rabbitmq-server_3.8.14-1_all.deb
+	fi
 fi
 if [[ $VERISON -eq 3 ]] 
 then
+	if [[ $WHAT -eq 2 ]]
+	then
+		scp -r $USERNAME@$SERVER:tentative/deployment/dependences $PWD/deploy
 
-	scp -r $USERNAME@$SERVER:tentative/deployment/dependences $PWD/deploy
+		#apt update 
+		dpkg -i $PWD/deploy/dependences/apacheds-2.0.0.AM26-amd64.deb
+		dpkg -i $PWD/deploy/dependences/php_7.2+60ubuntu1_all.deb
+		dpkg -i $PWD/deploy/dependences/mysql-apt-config_0.8.17-1_all.deb
+		dpkg -i $PWD/deploy/dependences/rabbitmq-server_3.8.14-1_all.deb
+		
+	elif [[ $WHAT -eq 1 ]]
+	then 
+		git clone https://github.com/jefuhr/tentative.git ./$PWD/deploy
+		
 
-	#apt update 
-	dpkg -i $PWD/deploy/dependences/apacheds-2.0.0.AM26-amd64.deb
-	dpkg -i $PWD/deploy/dependences/php_7.2+60ubuntu1_all.deb
-	dpkg -i $PWD/deploy/dependences/mysql-apt-config_0.8.17-1_all.deb
-	dpkg -i $PWD/deploy/dependences/rabbitmq-server_3.8.14-1_all.deb
+	fi
 fi
 echo "$TEST"
